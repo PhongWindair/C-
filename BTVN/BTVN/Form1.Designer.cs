f5de1ff6673e871c99599193a79aadc7b552930a
PhongWindair <tranphong120560@gmail.com>
2024-11-28 21:11:00 +0700

    Add files via upload

---
+++ BTVN/BTVN/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace BTVN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.add = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.txtlast = new System.Windows.Forms.TextBox();
+            this.txtfirst = new System.Windows.Forms.TextBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(117, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(441, 257);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Last Name";
+            this.columnHeader1.Width = 109;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 116;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            this.columnHeader3.Width = 105;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(141, 393);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 1;
+            this.add.Text = "add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(301, 393);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(75, 23);
+            this.del.TabIndex = 2;
+            this.del.Text = "del";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(458, 393);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(75, 23);
+            this.edit.TabIndex = 3;
+            this.edit.Text = "edit";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // txtlast
+            // 
+            this.txtlast.Location = new System.Drawing.Point(632, 84);
+            this.txtlast.Name = "txtlast";
+            this.txtlast.Size = new System.Drawing.Size(150, 22);
+            this.txtlast.TabIndex = 4;
+            // 
+            // txtfirst
+            // 
+            this.txtfirst.Location = new System.Drawing.Point(632, 160);
+            this.txtfirst.Name = "txtfirst";
+            this.txtfirst.Size = new System.Drawing.Size(150, 22);
+            this.txtfirst.TabIndex = 5;
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(632, 229);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(150, 22);
+            this.txtphone.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 512);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtfirst);
+            this.Controls.Add(this.txtlast);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.TextBox txtlast;
+        private System.Windows.Forms.TextBox txtfirst;
+        private System.Windows.Forms.TextBox txtphone;
+    }
+}
+
---
+++ BTVN/BTVN/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            ListViewItem newitem = new ListViewItem (txtlast.Text);
+            newitem.SubItems.Add (txtfirst.Text);
+            newitem.SubItems.Add (txtphone.Text);
+            listView1 .Items.Add (newitem);
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0) { 
+                txtlast.Text = listView1.SelectedItems[0].SubItems[0].Text;
+                txtfirst.Text = listView1.SelectedItems[0].SubItems[1].Text;
+                txtphone.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            }
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                listView1.SelectedItems[0].SubItems[0].Text = txtlast.Text;
+                listView1.SelectedItems[0].SubItems[1].Text = txtfirst.Text;
+                listView1.SelectedItems[0].SubItems[2].Text = txtphone.Text;
+
+
+            }
+        }
+    }
+}
